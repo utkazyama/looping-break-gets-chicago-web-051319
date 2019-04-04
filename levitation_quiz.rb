b60bcb6042b8678706e222723e86ad5c274e20b4
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-04 04:09:41 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -2,8 +2,8 @@
 def levitation_quiz
 	loop do
 	puts 'What is the spell that enacts levitation?'
-  
-	break if answer=="Wingardium Leviosa"
+  #answer=gets.chomp
+	#break if answer=="Wingardium Leviosa"
 	end
 	puts 'You passed the quiz!'
 	end
